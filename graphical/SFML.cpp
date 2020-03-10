1d2e07742c0ca213d0f5fb2783fcefe361faa329
Bouvier <vivien.bouvier@epitech.eu>
2020-03-10 14:43:00 +0100

    [OBJ] Dislaying Windows SFMl and ncurses

---
+++ graphical/SFML.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2020
+** epitech
+** File description:
+** cpp
+*/
+
+#include <SFML/Graphics.hpp>
+
+void Display_Window(int x, int y)
+{
+    sf::RenderWindow window(sf::VideoMode(x, y), "SFML works!");
+    sf::CircleShape shape(100.f);
+
+    shape.setFillColor(sf::Color::Green);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
