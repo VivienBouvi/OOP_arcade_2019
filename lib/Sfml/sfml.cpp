2e29071a3e85813eac2340177efb0975207a214e
Bouvier <vivien.bouvier@epitech.eu>
2020-04-02 16:30:26 +0200

    [OBJ] Snake done, starting lib sfml

--- lib/Sfml/sfml.cpp
+++ lib/Sfml/sfml.cpp
@@ -9,23 +9,12 @@
 
 extern "C"
 {
-    void Display_Window(int x, int y)
+    void Create_rect(int x, int y)
     {
-        sf::RenderWindow window(sf::VideoMode(x, y), "SFML works!");
-        sf::CircleShape shape(100.f);
+        sf::RectangleShape sprite;
 
-        shape.setFillColor(sf::Color::Green);
-        while (window.isOpen())
-        {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-            window.clear();
-            window.draw(shape);
-            window.display();
-        }
+        sprite.setSize(Vector2f(x, y));
+        sprite.setOutlineThickness(1.0f);
+
     }
 }
